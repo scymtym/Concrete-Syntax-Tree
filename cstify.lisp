ed5b3efde3e739c069a92750e0f6dc001e18f68b
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2018-05-07 18:56:12 +0200

    CSTIFY can handle improper lists

--- cstify.lisp
+++ cstify.lisp
@@ -1,11 +1,13 @@
 (cl:in-package #:concrete-syntax-tree)
 
-(defmethod cstify ((list cl:null))
-  (make-instance 'atom-cst :raw nil))
+;;; Handles NIL terminating a proper list as well as any other atom in
+;;; case of a dotted list.
+(defmethod cstify ((list t))
+  (make-instance 'atom-cst :raw list))
 
 (defmethod cstify ((list cl:cons))
-  (let ((rest (cstify (cdr list))))
-    (make-instance 'cons-cst
-      :first (car list)
-      :rest rest
-      :raw (cl:cons (raw (car list)) (raw rest)))))
+  (make-instance 'cons-cst
+                 :first (cl:car list)
+                 :rest (cstify (cdr list))
+                 :raw list #+no (cl:cons (raw (car list)) (raw rest))
+                 ))
