dca7eea82cf727752b822d5d57a7480e8f551741
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2018-05-07 19:02:16 +0200

    new condition type NULL-CST-REQUIRED

--- conditions.lisp
+++ conditions.lisp
@@ -3,6 +3,9 @@
 (define-condition cst-error (error acclimation:condition)
   ())
 
+(define-condition null-cst-required (cst-error)
+  ((%cst :initarg :cst :reader cst)))
+
 ;;; This condition is signaled whenever an instance of the class
 ;;; CONS-CST was required, but something else was given.
 (define-condition cons-cst-required (cst-error)
